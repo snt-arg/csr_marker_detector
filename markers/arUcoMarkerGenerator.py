bd5c5dcc8abf7bbcbd0390a5efda2ef59b796d57
alitourani <a.tourani1991@gmail.com>
2022-05-30 17:34:41 +0200

    feat: improve marker generator

--- markers/arUcoMarkerGenerator.py
+++ markers/arUcoMarkerGenerator.py
@@ -1,11 +1,10 @@
-import sys
 import cv2 as cv
 import numpy as np
 from utils.arUcoUtils import arUcoDictionary
 
-defaultGeneratedMarkersPath = './generatedMarkers'
+defaultGenPath = './generated-markers'
 
-def arUcoMarkerGenerator(id = 0, dictType = arUcoDictionary['DICT_ARUCO_ORIGINAL'], outputPath = defaultGeneratedMarkersPath):
+def arUcoMarkerGenerator(id=0, dictType='DICT_ARUCO_ORIGINAL', size=250, outputPath=defaultGenPath):
     """
     Generates a marker for ARUCO.
 
@@ -15,6 +14,8 @@ def arUcoMarkerGenerator(id = 0, dictType = arUcoDictionary['DICT_ARUCO_ORIGINAL
         The id of the marker (default 0).
     dictType : str
         The dictionary type of the marker (default '').
+    size : int
+        The size of the marker (default 250).
     outputPath : str
         The path to the output folder (default './generatedMarkers').
 
@@ -26,12 +27,18 @@ def arUcoMarkerGenerator(id = 0, dictType = arUcoDictionary['DICT_ARUCO_ORIGINAL
         # Check if the dictionary type is valid
         if arUcoDictionary[dictType] is None:
             print(f'Dictionary type "{dictType}" not found. Exiting ...')
-        # Otherwise, generate the marker
+        # Otherwise, catch the dictionary type
         selectedDict = cv.aruco.Dictionary_get(arUcoDictionary[dictType])
-        print(selectedDict)
+        # Create the marker
+        print(f"Creating marker #{id} of {dictType} ...")
+        tag = np.zeros((size, size, 1), dtype=np.uint8)
+        cv.aruco.drawMarker(selectedDict, id, size, tag, 1)
+        # Save the marker to the output folder
+        fileName = f'ArUco_{dictType}#{"{0:05d}".format(id)}.png'
+        cv.imwrite(f'{outputPath}/{fileName}', tag)
+        cv.imshow(fileName, tag)
+        cv.waitKey(0)
     except Exception as exception:
         print(f'Error occurred when generating the arUco marker!\n{exception}', 'error')
 
-arUcoMarkerGenerator(0, 'DICT_ARUCO_ORIGINAL', defaultGeneratedMarkersPath)
-
-# https://pyimagesearch.com/2020/12/14/generating-aruco-markers-with-opencv-and-python/
+arUcoMarkerGenerator(10, 'DICT_ARUCO_ORIGINAL')
